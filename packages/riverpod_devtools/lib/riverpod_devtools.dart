4f46a3b11959cc60b4f45018fdfbf524262ff468
yutsuki3 <yukihiro.terakado@gmail.com>
2025-12-29 16:00:26 +0900

    feat: Implement Riverpod DevTools observer to post provider events and update package metadata.

--- packages/riverpod_devtools/lib/riverpod_devtools.dart
+++ packages/riverpod_devtools/lib/riverpod_devtools.dart
@@ -1,8 +1,56 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
+library riverpod_devtools;
 
-export 'src/riverpod_devtools_base.dart';
+import 'dart:convert';
+import 'dart:developer' as developer;
 
-// TODO: Export any libraries intended for clients of this package.
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RiverpodDevToolsObserver extends ProviderObserver {
+  @override
+  void didAddProvider(
+    ProviderBase<Object?> provider,
+    Object? value,
+    ProviderContainer container,
+  ) {
+    _postEvent('provider_added', {
+      'provider': provider.name ?? provider.runtimeType.toString(),
+      'value': _serializeValue(value),
+    });
+  }
+
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    _postEvent('provider_updated', {
+      'provider': provider.name ?? provider.runtimeType.toString(),
+      'previousValue': _serializeValue(previousValue),
+      'newValue': _serializeValue(newValue),
+    });
+  }
+
+  @override
+  void didDisposeProvider(
+    ProviderBase<Object?> provider,
+    ProviderContainer container,
+  ) {
+    _postEvent('provider_disposed', {
+      'provider': provider.name ?? provider.runtimeType.toString(),
+    });
+  }
+
+  void _postEvent(String kind, Map<String, Object?> data) {
+    developer.postEvent('riverpod:$kind', data);
+  }
+
+  String _serializeValue(Object? value) {
+    try {
+      return jsonEncode(value);
+    } catch (_) {
+      return value.toString();
+    }
+  }
+}
